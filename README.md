816432ac32cadf0f27202a1e22255089d06ca52e
Aiden Chang <aidenchang@gmail.com>
2024-03-31 16:35:14 -0700

    readme

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# smallLLM
-Training my own LLM with little data
+# Generatively Pretrained Transformer Project
+
+This project is dedicated to building a Generatively Pretrained Transformer (GPT) inspired by the seminal paper "Attention is All You Need" and the advancements seen in OpenAI's GPT-2 and GPT-3 models. Our journey explores the intricate connections to ChatGPT, a model that has significantly impacted natural language processing and understanding. In a twist of meta-learning, we've also incorporated GitHub Copilot, a tool based on GPT, to assist in developing parts of this GPT model.
+
+## Background
+
+The project is grounded in the fundamentals of autoregressive language modeling, leveraging the power of tensors and the PyTorch neural network (nn) framework. For those new to these concepts, I highly recommend watching the "makemore" video series by Andrej Karpathy, which provides an excellent introduction to language models and PyTorch.
+
+## Repository Structure
+
+- **data/**: Contains datasets used for training the model.
+- **model/**: The core transformer model implementation.
+- **src/**: Source code for the project, including utility functions and model components.
+- **utils/**: Utility scripts for data preprocessing, model evaluation, and other tasks.
+- **.gitignore**: Specifies intentionally untracked files to ignore.
+- **LICENSE**: The project is open-sourced under the MIT license.
+- **README.md**: This document.
+- **evaluate.py**: Script for evaluating the trained model's performance.
+- **tokenize.ipynb**: Jupyter notebook detailing the tokenization process.
+- **train.py**: Script for training the GPT model.
+
+## Getting Started
+
+To get started with this project, clone the repository and install the required dependencies (listed in a requirements.txt file you should create based on your project's dependencies).
+
+```bash
+git clone https://github.com/aiden200/smallLLM.git
+cd smallLLM
+pip install -r requirements.txt
+```
+
+## Training the Model
+
+To train the model, run:
+```bash
+python train.py
+```
+
+## Credits
+This project was inspired by the work of Andrej Karpathy, particularly his "makemore" series on YouTube. His teachings on autoregressive language modeling and PyTorch are the best i've seen.
+
+Additionally, this project owes its inception to the foundational works of [Attention is All You Need](https://arxiv.org/abs/1706.03762) and the development of GPT-2 and GPT-3 by OpenAI.
--- train.py
+++ train.py
@@ -1,36 +1,81 @@
-map_char_to_int = None
-map_int_to_char = None
+from utils.vanilla_tokenizers import extract_encoder_decoder, test_encoder_decoder
+from utils.split_data import get_batch
+from model.bigram_model import Transformer_Model
+from tqdm import tqdm
+import torch
 
-def information_on_data(file):
-    with open(file, 'r', encoding= 'utf-8') as f:
-        txt = f.read()
-    
-    data_len = len(txt)
-    vocab = sorted(list(set(txt)))
-    set_encodings(vocab)
-    return data_len
 
-def set_encodings(vocab):
-    #very basic level of encodings
-    global map_char_to_int, map_int_to_char
-    map_char_to_int = { ch:i for i,ch in enumerate(vocab)} 
-    map_int_to_char = { i:ch for i,ch in enumerate(vocab)} 
+@torch.no_grad()  # doesn't calculate gradients. We will not call .backward()
+def estimate_loss(model, train_data, val_data, eval_iters, device, batch_size, max_length):
+    out = {}
+    model.eval()  # layers like batchnorm and dropout won't be applied
+    for split in ['train', 'val']:
+        data = train_data if split == "train" else val_data
+        losses = torch.zeros(eval_iters)
+        for i in range(eval_iters):
+            X, Y = get_batch(data, batch_size, max_length, device)
+            logits, loss = model(X, Y)
+            losses[i] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
-def encode(text):
-    #character level tokenizer, long long tokens but easy and simple
-    if map_char_to_int == None:
-        print("Encodings not set")
-        exit(0)
-    encodings = []
-    for char in text:
-        encodings.append(map_char_to_int[char])
-    return encodings
 
-def decode(text):
-    if map_int_to_char == None:
-        print("Decodings not set")
-        exit(0)
-    decodings = ""
-    for number in text:
-        decodings = decodings + map_int_to_char[number]
-    return decodings
+def train():
+    filename = "data/input.txt"
+
+    encoder, decoder, text, vocab_size = extract_encoder_decoder(filename)
+    data = torch.tensor(encoder(text), dtype=torch.long)
+
+    split = int(0.9*len(data))
+    train = data[:split]
+    val = data[split:]
+
+    max_length = 256  # x = 0 y = 1, x = 0, 1 y = 2, x = 0,1,2 y =3 ... so on till x is len 8
+    batch_size = 64
+    lr = 3e-4
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    epochs = 5000
+    eval_interval = 500
+    embed_size = 384
+    attention_heads = 6
+    dropout = 0.2
+
+    m = Transformer_Model(vocab_size, embed_size, max_length,
+                          attention_heads, dropout, device)
+    m = m.to(device)
+
+    optimizer = torch.optim.AdamW(m.parameters(), lr=lr)
+    for e in tqdm(range(epochs)):
+
+        # every eval_interval, eval how we are doing on the loss
+        if e % eval_interval == 0:
+            losses = estimate_loss(
+                m, train, val, eval_interval, device, batch_size, max_length)
+            print(
+                f"step {e}: train loss: {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+        train_x_batch, train_y_batch = get_batch(
+            train, batch_size, max_length, device)
+        logits, loss = m(train_x_batch, train_y_batch)
+
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    print(loss)
+
+    # val_x_batch, val_y_batch = get_batch(val, batch_size, max_length)
+
+    logits, loss = m(train_x_batch, train_y_batch)
+
+    print(logits.shape)
+    # start index with (B,1)
+    start_idx = torch.zeros((batch_size, 1), dtype=torch.long, device=device)
+
+    # Generate 5 chars and grab first row
+    output = m.generate(start_idx, 250)[0].tolist()
+    print("".join(decoder(output)))
+
+
+train()
